2f488ae2c28a1094e8f0e3a754ea0c92dc3b6e5d
Andrew Dorney <andrewd18@gmail.com>
2013-02-15 14:41:42 -0600

    Get rid of glade from system_view_planet_overview_widget.

--- view/system_view_planet_overview_widget.rb
+++ view/system_view_planet_overview_widget.rb
@@ -4,31 +4,36 @@ require 'gtk3'
 # This widget is designed to show a single planet's image, its name, and its alias.
 class SystemViewPlanetOverviewWidget < Gtk::Box
   def initialize(planet)
-	super(:horizontal)
+	super(:vertical)
 	
 	# Hook up our model data.
-	@planet = planet
+	@planet_model = planet
+	@planet_model.add_observer(self)
 	
-	@planet.add_observer(self)
+	@planet_image_event_wrapper = Gtk::EventBox.new
+	@planet_image_event_wrapper.events = Gdk::Event::BUTTON_PRESS_MASK
 	
-	builder = Gtk::Builder.new
+	@planet_image = Gtk::Image.new("view/images/extractor_icon.svg")
+	@planet_image_event_wrapper.add(@planet_image)
 	
-	builder.add_from_file("view/system_view_planet_overview_widget.glade")
-	
-	@planet_image = builder.get_object('planet_image')
-	@planet_name_label = builder.get_object('planet_name_label')
-	@planet_alias_label = builder.get_object('planet_alias_label')
-	@delete_planet_button = builder.get_object('delete_planet_button')
-	
-	update_image_name_and_alias
-	
-	vertical_layout = builder.get_object('vertical_layout')
+	@planet_name_label = Gtk::Label.new("#{@planet_model.name}" || "")
+	@planet_alias_label = Gtk::Label.new("#{@planet_model.planet_alias}" || "")
+	@delete_planet_button = Gtk::Button.new(:stock_id => Gtk::Stock::DELETE)
 	
 	# Pack the completed widget into ourself.
-	self.pack_start(vertical_layout)
+	self.pack_start(@planet_image_event_wrapper)
+	self.pack_start(@planet_name_label)
+	self.pack_start(@planet_alias_label)
+	self.pack_start(@delete_planet_button)
 	
-	# Connect all the back-end signals.
-	builder.connect_signals { |handler| self.method(handler) }
+	# Connect the backend signals.
+	@delete_planet_button.signal_connect("clicked") do
+	  on_delete_planet_button_clicked
+	end
+	
+	@planet_image_event_wrapper.signal_connect("button_press_event") do |main_window, event|
+	  on_planet_image_button_press_event(event)
+	end
 	
 	# Show child widgets.
 	self.show_all
@@ -43,6 +48,17 @@ class SystemViewPlanetOverviewWidget < Gtk::Box
   
   def on_delete_planet_button_clicked
 	@planet.remove_planet
+	self.destroy
+  end
+  
+  def on_planet_image_button_press_event(event)
+	puts "on_planet_image_button_press_event"
+	
+	if event.event_type == Gdk::Event::BUTTON2_PRESS
+	  puts "Double-click!"
+	else
+	  puts "Not double-click."
+	end
   end
   
   private
@@ -50,7 +66,7 @@ class SystemViewPlanetOverviewWidget < Gtk::Box
   # Update image, name, and alias values from the model.
   def update_image_name_and_alias
 	# planet_image.image = planet.image
-	@planet_name_label.text = @planet.name || ""
-	@planet_alias_label.text = @planet.planet_alias || ""
+	@planet_name_label.text = @planet_model.name || ""
+	@planet_alias_label.text = @planet_model.planet_alias || ""
   end
 end
