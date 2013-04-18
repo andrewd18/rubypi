c73027c711a9e5eeaaf595746e814ca09359af72
Andrew Dorney <andrewd18@gmail.com>
2013-04-18 11:54:39 -0500

    Add a test suite for all the model tests.

---
+++ tests/test_suite_model_tests.rb
@@ -0,0 +1,7 @@
+require 'test/unit'
+
+# Model Tests
+require_relative './test_case_command_center.rb'
+require_relative './test_case_basic_industrial_facility.rb'
+require_relative './test_case_launchpad.rb'
+require_relative './test_case_storage_facility.rb'
