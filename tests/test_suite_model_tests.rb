e746f9170815f7c03ee24e6ee6ed6c911e93c45a
Andrew Dorney <andrewd18@gmail.com>
2013-04-23 16:51:15 -0500

    Add high tech industrial facility tests to the "rake test" command.

--- tests/test_suite_model_tests.rb
+++ tests/test_suite_model_tests.rb
@@ -6,6 +6,7 @@ require 'test/unit'
 require_relative './test_case_command_center.rb'
 require_relative './test_case_basic_industrial_facility.rb'
 require_relative './test_case_advanced_industrial_facility.rb'
+require_relative './test_case_high_tech_industrial_facility.rb'
 require_relative './test_case_launchpad.rb'
 require_relative './test_case_storage_facility.rb'
 
