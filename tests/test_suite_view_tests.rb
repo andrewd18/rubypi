7a656750d37dac4bfaf37c3118495d32a7ce72c7
Andrew Dorney <andrewd18@gmail.com>
2013-04-30 09:10:05 -0500

    Add test for system stats widget.

--- tests/test_suite_view_tests.rb
+++ tests/test_suite_view_tests.rb
@@ -5,6 +5,7 @@ require 'test/unit'
 # System Widget and subwidgets.
 require_relative './test_case_system_view_widget.rb'
 require_relative './test_case_system_view_planet_overview_widget.rb'
+require_relative './test_case_system_stats_widget.rb'
 
 # Planet View Widget and subwidgets.
 
