1e29cae9c801a1facb2c329515aa90db42da2a38
Andrew Dorney <andrewd18@gmail.com>
2013-04-30 10:55:48 -0500

    Revbump to 0.0.4.

--- view/ruby_pi_about_dialog.rb
+++ view/ruby_pi_about_dialog.rb
@@ -6,7 +6,7 @@ class RubyPIAboutDialog < Gtk::AboutDialog
 	
 	self.name = "About RubyPI"
 	self.program_name = "RubyPI"
-	self.version = "0.0.3"
+	self.version = "0.0.4"
 	self.copyright = "Copyright (C) 2013, Andrew Dorney <andrewd18@gmail.com>"
 
 	gplv3 = "This program is free software: you can redistribute it and/or modify "
