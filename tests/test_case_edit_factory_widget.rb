3e79421487d2c21dd568ffcfc7f3bc31b91b08a2
Andrew Dorney <andrewd18@gmail.com>
2013-04-30 10:19:58 -0500

    Add test cases for EditFactoryWidget.

---
+++ tests/test_case_edit_factory_widget.rb
@@ -0,0 +1,77 @@
+require "test/unit"
+
+require_relative "../view/edit_factory_widget.rb"
+require_relative "../model/basic_industrial_facility.rb"
+require_relative "../model/advanced_industrial_facility.rb"
+require_relative "../model/high_tech_industrial_facility.rb"
+
+class TestCaseEditFactoryWidget < Test::Unit::TestCase
+  # Run once.
+  def self.startup
+  end
+  
+  # Run once after all tests.
+  def self.shutdown
+  end
+  
+  # Run before every test.
+  def setup
+	@basic_industrial_facility = BasicIndustrialFacility.new
+	@advanced_industrial_facility = AdvancedIndustrialFacility.new
+	@high_tech_industrial_facility = HighTechIndustrialFacility.new
+	
+	@edit_factory_widget = EditFactoryWidget.new(@basic_industrial_facility)
+  end
+  
+  # Run after every test.
+  def teardown
+  end
+  
+  def test_can_be_asked_about_its_model_object
+	assert_equal(@basic_industrial_facility, @edit_factory_widget.building_model)
+  end
+  
+  def test_does_not_observe_model_object_by_default
+	assert_equal(0, @edit_factory_widget.building_model.count_observers)
+  end
+  
+  def test_can_be_told_to_start_observing_model_object
+	assert_equal(0, @edit_factory_widget.building_model.count_observers)
+	
+	@edit_factory_widget.start_observing_model
+	
+	# One observer for self.
+	assert_equal(1, @edit_factory_widget.building_model.count_observers)
+  end
+  
+  def test_can_be_told_to_stop_observing_model_object
+	assert_equal(0, @edit_factory_widget.building_model.count_observers)
+	
+	@edit_factory_widget.start_observing_model
+	
+	# One observer for self.
+	assert_equal(1, @edit_factory_widget.building_model.count_observers)
+	
+	@edit_factory_widget.stop_observing_model
+	
+	assert_equal(0, @edit_factory_widget.building_model.count_observers)
+  end
+  
+  def test_can_be_assigned_new_model_object
+	@edit_factory_widget.building_model = @advanced_industrial_facility
+	
+	assert_false(@edit_factory_widget.building_model == @basic_industrial_facility)
+	assert_equal(@advanced_industrial_facility, @edit_factory_widget.building_model)
+  end
+  
+  def test_when_destroyed_unhooks_observers
+	@edit_factory_widget.start_observing_model
+	
+	# One observer for self.
+	assert_equal(1, @basic_industrial_facility.count_observers)
+	
+	@edit_factory_widget.destroy
+	
+	assert_equal(0, @basic_industrial_facility.count_observers)
+  end
+end
--- view/edit_factory_widget.rb
+++ view/edit_factory_widget.rb
@@ -4,12 +4,14 @@ require_relative '../model/schematic.rb'
 
 # This widget provides all the options necessary to edit a BasicIndustrialFacility, AdvancedIndustrialFacility, or HighTechIndustrialFacility.
 class EditFactoryWidget < Gtk::Box
-  def initialize(factory_model)
+  
+  attr_accessor :building_model
+  
+  def initialize(building_model)
 	super(:vertical)
 	
 	# Hook up model data.
-	@factory_model = factory_model
-	@factory_model.add_observer(self)
+	@building_model = building_model
 	
 	# Gtk::Table Syntax
 	# table = Gtk::Table.new(rows, columns)
@@ -23,7 +25,7 @@ class EditFactoryWidget < Gtk::Box
 	
 	# Populate the combo box with the schematics this factory can accept.
 	@schematic_combo_box = Gtk::ComboBoxText.new
-	@factory_model.accepted_schematic_names.each do |name|
+	@building_model.accepted_schematic_names.each do |name|
 	  @schematic_combo_box.append_text(name)
 	end
 	
@@ -43,17 +45,25 @@ class EditFactoryWidget < Gtk::Box
 	return self
   end
   
-  # Called when the factory_model changes.
+  def start_observing_model
+	@building_model.add_observer(self)
+  end
+  
+  def stop_observing_model
+	@building_model.delete_observer(self)
+  end
+  
+  # Called when the building_model changes.
   def update
 	# Don't update the Gtk/Glib C object if it's in the process of being destroyed.
 	unless (self.destroyed?)
 	  # Set the active schematic combo box iterator to the model's schematic.
-	  if (@factory_model.schematic_name == nil)
+	  if (@building_model.schematic_name == nil)
 		@schematic_combo_box.active_iter=(nil)
 	  else
 		# Find the iter that corresponds to the model's schematic.
 		@schematic_combo_box.model.each do |model, path, iter|
-		  if (@factory_model.schematic_name == iter.get_value(0))
+		  if (@building_model.schematic_name == iter.get_value(0))
 			@schematic_combo_box.active_iter=(iter)
 		  end
 		end
@@ -63,7 +73,7 @@ class EditFactoryWidget < Gtk::Box
   
   def commit_to_model
 	# Stop observing so the values we want to set don't get overwritten on an #update.
-	@factory_model.delete_observer(self)
+	self.stop_observing_model
 	
 	# Ignore commit unless the user picked something legit.
 	if (@schematic_combo_box.active_iter == nil)
@@ -72,23 +82,23 @@ class EditFactoryWidget < Gtk::Box
 	  currently_selected_schematic_name = @schematic_combo_box.active_iter.get_value(0)
 	  
 	  # Find the schematic that corresponds to the active iterator.
-	  @factory_model.accepted_schematic_names.each do |name|
+	  @building_model.accepted_schematic_names.each do |name|
 		if (name == currently_selected_schematic_name)
-		  @factory_model.schematic_name = name
+		  @building_model.schematic_name = name
 		end
 	  end
 	end
 	
 	# Start observing again.
-	@factory_model.add_observer(self)
+	self.start_observing_model
   end
   
   def destroy
+	self.stop_observing_model
+	
 	self.children.each do |child|
 	  child.destroy
 	end
-	
-	@factory_model.delete_observer(self)
 	
 	super
   end
