ea138ac19ab26f09df1481bc56fed8aa2d918753
Andrew Dorney <andrewd18@gmail.com>
2013-05-07 09:49:08 -0500

    Change name from Mock to Stub, per discussion at http://stackoverflow.com/questions/3459287/whats-the-difference-between-a-mock-stub.
    Apparently mocks can tell if they've been tested or not. Stubs are just sample code. Welp.

--- tests/test_case_unrestricted_storage.rb
+++ tests/test_case_unrestricted_storage.rb
@@ -4,7 +4,7 @@ require "test/unit"
 require "observer"
 require_relative "../model/unrestricted_storage.rb"
 
-class UnrestrictedStorageMock
+class UnrestrictedStorageStub
   include Observable
   include UnrestrictedStorage
   
@@ -13,7 +13,7 @@ class UnrestrictedStorageMock
   end
 end
 
-class NotObservableUnrestrictedStorageMock
+class NotObservableUnrestrictedStorageStub
   include UnrestrictedStorage
   
   def storage_volume
@@ -36,7 +36,7 @@ class TestCaseUnrestrictedStorage < Test::Unit::TestCase
 
   # Run before every test.
   def setup
-	@building_mock = UnrestrictedStorageMock.new
+	@building_stub = UnrestrictedStorageStub.new
 	@was_notified_of_change = false
   end
   
@@ -48,254 +48,254 @@ class TestCaseUnrestrictedStorage < Test::Unit::TestCase
   def test_building_can_store_any_product_regardless_of_p_level # unlike a factory
 	hash_with_dwarves_and_ancient_beast = {"Dwarf" => 999, "Ancient Beast" => 1}
 	
-	@building_mock.store_product("Dwarf", 999)
-	@building_mock.store_product("Ancient Beast", 1)
+	@building_stub.store_product("Dwarf", 999)
+	@building_stub.store_product("Ancient Beast", 1)
 	
-	assert_equal(hash_with_dwarves_and_ancient_beast, @building_mock.stored_products)
+	assert_equal(hash_with_dwarves_and_ancient_beast, @building_stub.stored_products)
   end
   
   def test_building_can_store_any_amount_of_a_given_product_within_max_volume # unlike a factory
 	# This is the max number of P0s you can put in a 500.0 m3 building.
 	hash_with_50k_dwarves = {"Dwarf" => 50000}
 	
-	@building_mock.store_product("Dwarf", 50000)
+	@building_stub.store_product("Dwarf", 50000)
 	
-	assert_equal(hash_with_50k_dwarves, @building_mock.stored_products)
+	assert_equal(hash_with_50k_dwarves, @building_stub.stored_products)
   end
   
   def test_building_can_add_a_certain_amount_of_a_specific_product_to_its_stores
 	hash_with_5_dwarves = {"Dwarf" => 5}
 	
-	@building_mock.store_product("Dwarf", 5)
+	@building_stub.store_product("Dwarf", 5)
 	
-	assert_equal(hash_with_5_dwarves, @building_mock.stored_products)
+	assert_equal(hash_with_5_dwarves, @building_stub.stored_products)
   end
   
   def test_building_stacks_products_if_you_add_more
 	hash_with_5_dwarves = {"Dwarf" => 5}
 	hash_with_20_dwarves = {"Dwarf" => 20}
 	
-	@building_mock.store_product("Dwarf", 5)
+	@building_stub.store_product("Dwarf", 5)
 	
-	assert_equal(hash_with_5_dwarves, @building_mock.stored_products)
+	assert_equal(hash_with_5_dwarves, @building_stub.stored_products)
 	
 	# Add 15 more.
-	@building_mock.store_product("Dwarf", 15)
+	@building_stub.store_product("Dwarf", 15)
 	
-	assert_equal(hash_with_20_dwarves, @building_mock.stored_products)
+	assert_equal(hash_with_20_dwarves, @building_stub.stored_products)
   end
   
   def test_error_when_adding_a_product_and_first_arg_is_not_a_string
 	assert_raise do
-	  @building_mock.store_product(@@dwarf, 5)
+	  @building_stub.store_product(@@dwarf, 5)
 	end
 	
 	# Make sure the building didn't change.
 	empty_hash = {}
-	assert_equal(empty_hash, @building_mock.stored_products)
+	assert_equal(empty_hash, @building_stub.stored_products)
   end
   
   def test_error_when_adding_a_product_and_first_arg_is_not_a_registered_product
 	assert_raise do
-	  @building_mock.store_product("Macaque", 5)
+	  @building_stub.store_product("Macaque", 5)
 	end
 	
 	# Make sure the building didn't change.
 	empty_hash = {}
-	assert_equal(empty_hash, @building_mock.stored_products)
+	assert_equal(empty_hash, @building_stub.stored_products)
   end
   
   def test_error_when_adding_a_product_and_second_arg_is_not_a_number
 	assert_raise do
-	  @building_mock.store_product("Dwarf", "Hammerdwarf")
+	  @building_stub.store_product("Dwarf", "Hammerdwarf")
 	end
 	
 	# Make sure the building didn't change.
 	empty_hash = {}
-	assert_equal(empty_hash, @building_mock.stored_products)
+	assert_equal(empty_hash, @building_stub.stored_products)
   end
   
   def test_building_can_add_a_certain_amount_of_a_specific_product_to_its_stores_by_product_instance
-	@building_mock.store_product_instance(@@dwarf, 5)
+	@building_stub.store_product_instance(@@dwarf, 5)
 	
 	hash_with_dwarf = {"Dwarf" => 5}
 	
-	assert_equal(hash_with_dwarf, @building_mock.stored_products)
+	assert_equal(hash_with_dwarf, @building_stub.stored_products)
   end
   
   def test_building_stacks_products_if_you_add_more_by_instance
 	hash_with_5_dwarves = {"Dwarf" => 5}
 	hash_with_20_dwarves = {"Dwarf" => 20}
 	
-	@building_mock.store_product_instance(@@dwarf, 5)
+	@building_stub.store_product_instance(@@dwarf, 5)
 	
-	assert_equal(hash_with_5_dwarves, @building_mock.stored_products)
+	assert_equal(hash_with_5_dwarves, @building_stub.stored_products)
 	
 	# Add 15 more.
-	@building_mock.store_product_instance(@@dwarf, 15)
+	@building_stub.store_product_instance(@@dwarf, 15)
 	
-	assert_equal(hash_with_20_dwarves, @building_mock.stored_products)
+	assert_equal(hash_with_20_dwarves, @building_stub.stored_products)
   end
   
   def test_error_when_adding_a_product_by_instance_and_first_arg_is_not_a_product
 	assert_raise do
-	  @building_mock.store_product_instance("Dwarf", 5)
+	  @building_stub.store_product_instance("Dwarf", 5)
 	end
 	
 	# Make sure the building didn't change.
 	empty_hash = {}
-	assert_equal(empty_hash, @building_mock.stored_products)
+	assert_equal(empty_hash, @building_stub.stored_products)
   end
   
   def test_error_when_adding_a_product_by_instance_and_second_arg_is_not_a_number
 	assert_raise do
-	  @building_mock.store_product_instance(@@dwarf, "Hammerdwarf")
+	  @building_stub.store_product_instance(@@dwarf, "Hammerdwarf")
 	end
 	
 	# Make sure the building didn't change.
 	empty_hash = {}
-	assert_equal(empty_hash, @building_mock.stored_products)
+	assert_equal(empty_hash, @building_stub.stored_products)
   end
   
   def test_building_can_remove_all_of_a_specific_product_from_its_stores
-	@building_mock.store_product("Dwarf", 5)
+	@building_stub.store_product("Dwarf", 5)
 	
 	hash_with_dwarf = {"Dwarf" => 5}
 	empty_hash = {}
 	
-	assert_equal(hash_with_dwarf, @building_mock.stored_products)
+	assert_equal(hash_with_dwarf, @building_stub.stored_products)
 	
-	@building_mock.remove_all_of_product("Dwarf")
+	@building_stub.remove_all_of_product("Dwarf")
 	
-	assert_equal(empty_hash, @building_mock.stored_products)
+	assert_equal(empty_hash, @building_stub.stored_products)
   end
   
   def test_building_can_remove_a_certain_amount_of_a_specific_product_from_its_stores
-	@building_mock.store_product("Dwarf", 5)
+	@building_stub.store_product("Dwarf", 5)
 	
 	hash_with_5_dwarves = {"Dwarf" => 5}
 	hash_with_3_dwarves = {"Dwarf" => 3}
 	
-	assert_equal(hash_with_5_dwarves, @building_mock.stored_products)
+	assert_equal(hash_with_5_dwarves, @building_stub.stored_products)
 	
-	@building_mock.remove_qty_of_product("Dwarf", 2)
+	@building_stub.remove_qty_of_product("Dwarf", 2)
 	
-	assert_equal(hash_with_3_dwarves, @building_mock.stored_products)
+	assert_equal(hash_with_3_dwarves, @building_stub.stored_products)
   end
   
   def test_building_errors_if_adding_a_product_would_overflow_it
 	# This is the max number of P0s you can put in a 500.0 m3 building.
 	hash_with_50k_dwarves = {"Dwarf" => 50000}
 	
-	@building_mock.store_product("Dwarf", 50000)
-	assert_equal(0.0, @building_mock.volume_available)
+	@building_stub.store_product("Dwarf", 50000)
+	assert_equal(0.0, @building_stub.volume_available)
 	
-	assert_equal(hash_with_50k_dwarves, @building_mock.stored_products)
+	assert_equal(hash_with_50k_dwarves, @building_stub.stored_products)
 	
 	# Even one more dwarf would overflow it. Make sure it errors.
 	assert_raise do
-	  @building_mock.store_product("Dwarf", 1)
+	  @building_stub.store_product("Dwarf", 1)
 	end
 	
 	# Make sure building doesn't change.
-	assert_equal(hash_with_50k_dwarves, @building_mock.stored_products)
+	assert_equal(hash_with_50k_dwarves, @building_stub.stored_products)
 	
 	# An Ancient Beast (P1), being larger than a dwarf (P0) obviously should overflow too.
 	assert_raise do
-	  @building_mock.store_product("Ancient Beast", 1)
+	  @building_stub.store_product("Ancient Beast", 1)
 	end
 	
 	# Make sure building doesn't change.
-	assert_equal(hash_with_50k_dwarves, @building_mock.stored_products)
+	assert_equal(hash_with_50k_dwarves, @building_stub.stored_products)
   end
   
   def test_building_errors_if_removing_a_product_that_doesnt_exist
 	empty_hash = {}
-	assert_equal(empty_hash, @building_mock.stored_products)
+	assert_equal(empty_hash, @building_stub.stored_products)
 	
 	assert_raise do
-	  @building_mock.remove_all_of_product("Dwarf")
+	  @building_stub.remove_all_of_product("Dwarf")
 	end
 	
-	assert_equal(empty_hash, @building_mock.stored_products)
+	assert_equal(empty_hash, @building_stub.stored_products)
 	
 	assert_raise do
-	  @building_mock.remove_qty_of_product("Dwarf", 2)
+	  @building_stub.remove_qty_of_product("Dwarf", 2)
 	end
 	
-	assert_equal(empty_hash, @building_mock.stored_products)
+	assert_equal(empty_hash, @building_stub.stored_products)
   end
   
   def test_building_removes_product_completely_if_more_than_value_is_removed
 	hash_with_5_dwarves = {"Dwarf" => 5}
 	empty_hash = {}
 	
-	assert_equal(empty_hash, @building_mock.stored_products)
+	assert_equal(empty_hash, @building_stub.stored_products)
 	
-	@building_mock.store_product("Dwarf", 5)
+	@building_stub.store_product("Dwarf", 5)
 	
-	assert_equal(hash_with_5_dwarves, @building_mock.stored_products)
+	assert_equal(hash_with_5_dwarves, @building_stub.stored_products)
 	
 	# Remove more dwarves than we have.
-	@building_mock.remove_qty_of_product("Dwarf", 20)
+	@building_stub.remove_qty_of_product("Dwarf", 20)
 	
 	# Shouldn't be at -15 dwarves.
-	assert_equal(empty_hash, @building_mock.stored_products)
+	assert_equal(empty_hash, @building_stub.stored_products)
   end
   
   def test_building_can_show_total_volume
 	# Default
-	assert_equal(500.0, @building_mock.total_volume)
+	assert_equal(500.0, @building_stub.total_volume)
 	
 	# After adding a product.
-	@building_mock.store_product("Dwarf", 1)
-	assert_equal(500.0, @building_mock.total_volume)
+	@building_stub.store_product("Dwarf", 1)
+	assert_equal(500.0, @building_stub.total_volume)
 	
 	# After adding second, different p-level product.
-	@building_mock.store_product("Ancient Beast", 1)
-	assert_equal(500.0, @building_mock.total_volume)
+	@building_stub.store_product("Ancient Beast", 1)
+	assert_equal(500.0, @building_stub.total_volume)
   end
   
   def test_building_can_show_volume_available
 	# Default
-	assert_equal(500.0, @building_mock.volume_available)
+	assert_equal(500.0, @building_stub.volume_available)
 	
 	# After adding a product.
 	# P-level 0 products should take up 0.01 per unit.
-	@building_mock.store_product("Dwarf", 1)
-	assert_equal(499.99, @building_mock.volume_available)
+	@building_stub.store_product("Dwarf", 1)
+	assert_equal(499.99, @building_stub.volume_available)
 	
 	# After adding second, different p-level product.
 	# P-level 1 products should take up 0.38 per unit.
-	@building_mock.store_product("Ancient Beast", 1)
-	assert_equal(499.61, @building_mock.volume_available)
+	@building_stub.store_product("Ancient Beast", 1)
+	assert_equal(499.61, @building_stub.volume_available)
   end
   
   def test_building_can_show_volume_used
 	# Default
-	assert_equal(0.0, @building_mock.volume_used)
+	assert_equal(0.0, @building_stub.volume_used)
 	
 	# After adding a product.
 	# P-level 0 products should take up 0.01 per unit.
-	@building_mock.store_product("Dwarf", 1)
-	assert_equal(0.01, @building_mock.volume_used)
+	@building_stub.store_product("Dwarf", 1)
+	assert_equal(0.01, @building_stub.volume_used)
 	
 	# After adding second, different p-level product.
 	# P-level 1 products should take up 0.38 per unit.
-	@building_mock.store_product("Ancient Beast", 1)
-	assert_equal(0.39, @building_mock.volume_used)
+	@building_stub.store_product("Ancient Beast", 1)
+	assert_equal(0.39, @building_stub.volume_used)
   end
     
   
   # Observer interaction tests.
   
-  def test_unrestricted_storage_mock_is_observable
-	assert_true(@building_mock.is_a?(Observable), "UnrestrictedStorageMock did not include Observable.")
+  def test_unrestricted_storage_stub_is_observable
+	assert_true(@building_stub.is_a?(Observable), "UnrestrictedStorageStub did not include Observable.")
   end
   
-  def test_non_observable_unrestricted_storage_mock_is_not_observable
-	@building_mock = NotObservableUnrestrictedStorageMock.new
-	assert_false(@building_mock.is_a?(Observable), "NotObservableUnrestrictedStorageMock did include Observable.")
+  def test_non_observable_unrestricted_storage_stub_is_not_observable
+	@building_stub = NotObservableUnrestrictedStorageStub.new
+	assert_false(@building_stub.is_a?(Observable), "NotObservableUnrestrictedStorageStub did include Observable.")
   end
   
   # Update method for testing observer.
@@ -305,141 +305,141 @@ class TestCaseUnrestrictedStorage < Test::Unit::TestCase
   
   def test_building_notifies_observers_on_store_product
 	assert_false(@was_notified_of_change)
-	@building_mock.add_observer(self)
+	@building_stub.add_observer(self)
 	
-	@building_mock.store_product("Dwarf", 1)
+	@building_stub.store_product("Dwarf", 1)
 	assert_true(@was_notified_of_change)
 	
-	@building_mock.delete_observer(self)
+	@building_stub.delete_observer(self)
   end
   
   def test_building_does_not_notify_observers_if_store_product_fails
 	assert_false(@was_notified_of_change)
-	@building_mock.add_observer(self)
+	@building_stub.add_observer(self)
 	
 	# Invalid product name.
 	assert_raise do
-	  @building_mock.store_product(@@dwarf, 1)
+	  @building_stub.store_product(@@dwarf, 1)
 	end
 	assert_false(@was_notified_of_change)
 	
 	# Invalid quantity.
 	assert_raise do
-	  @building_mock.store_product("Dwarf", "Fail")
+	  @building_stub.store_product("Dwarf", "Fail")
 	end
 	assert_false(@was_notified_of_change)
 	
 	# Too many products.
 	assert_raise do
-	  @building_mock.store_product("Dwarf", 99999999999999999)
+	  @building_stub.store_product("Dwarf", 99999999999999999)
 	end
 	assert_false(@was_notified_of_change)
 	
-	@building_mock.delete_observer(self)
+	@building_stub.delete_observer(self)
   end
   
   def test_building_notifies_observers_on_store_product_instance
 	assert_false(@was_notified_of_change)
-	@building_mock.add_observer(self)
+	@building_stub.add_observer(self)
 	
-	@building_mock.store_product_instance(@@dwarf, 1)
+	@building_stub.store_product_instance(@@dwarf, 1)
 	assert_true(@was_notified_of_change)
 	
-	@building_mock.delete_observer(self)
+	@building_stub.delete_observer(self)
   end
   
   def test_building_does_not_notify_observers_if_store_product_instance_fails
 	assert_false(@was_notified_of_change)
-	@building_mock.add_observer(self)
+	@building_stub.add_observer(self)
 	
 	# Invalid product instance.
 	assert_raise do
-	  @building_mock.store_product_instance("Dwarf", 1)
+	  @building_stub.store_product_instance("Dwarf", 1)
 	end
 	assert_false(@was_notified_of_change)
 	
 	# Invalid quantity.
 	assert_raise do
-	  @building_mock.store_product_instance(@@dwarf, "Fail")
+	  @building_stub.store_product_instance(@@dwarf, "Fail")
 	end
 	assert_false(@was_notified_of_change)
 	
 	# Too many products.
 	assert_raise do
-	  @building_mock.store_product_instance(@@dwarf, 99999999999999999)
+	  @building_stub.store_product_instance(@@dwarf, 99999999999999999)
 	end
 	assert_false(@was_notified_of_change)
 	
-	@building_mock.delete_observer(self)
+	@building_stub.delete_observer(self)
   end
   
   def test_building_notifies_observers_on_product_remove_all
 	assert_false(@was_notified_of_change)
-	@building_mock.store_product("Dwarf", 10)
+	@building_stub.store_product("Dwarf", 10)
 	
-	@building_mock.add_observer(self)
+	@building_stub.add_observer(self)
 	
-	@building_mock.remove_all_of_product("Dwarf")
+	@building_stub.remove_all_of_product("Dwarf")
 	assert_true(@was_notified_of_change)
 	
-	@building_mock.delete_observer(self)
+	@building_stub.delete_observer(self)
   end
   
   def test_building_does_not_notify_observers_if_product_remove_all_fails
 	assert_false(@was_notified_of_change)
-	@building_mock.store_product("Dwarf", 10)
+	@building_stub.store_product("Dwarf", 10)
 	
-	@building_mock.add_observer(self)
+	@building_stub.add_observer(self)
 	
 	assert_raise do
-	  @building_mock.remove_all_of_product("Ancient Beast")
+	  @building_stub.remove_all_of_product("Ancient Beast")
 	end
 	assert_false(@was_notified_of_change)
 	
-	@building_mock.delete_observer(self)
+	@building_stub.delete_observer(self)
   end
   
   def test_building_notifies_observers_on_product_remove_quantity
 	assert_false(@was_notified_of_change)
-	@building_mock.store_product("Dwarf", 10)
+	@building_stub.store_product("Dwarf", 10)
 	
-	@building_mock.add_observer(self)
+	@building_stub.add_observer(self)
 	
-	@building_mock.remove_qty_of_product("Dwarf", 5)
+	@building_stub.remove_qty_of_product("Dwarf", 5)
 	assert_true(@was_notified_of_change)
 	
-	@building_mock.delete_observer(self)
+	@building_stub.delete_observer(self)
   end
   
   def test_building_does_not_notify_observers_if_product_remove_quantity_fails
 	assert_false(@was_notified_of_change)
-	@building_mock.store_product("Dwarf", 10)
+	@building_stub.store_product("Dwarf", 10)
 	
-	@building_mock.add_observer(self)
+	@building_stub.add_observer(self)
 	
 	assert_raise do
-	  @building_mock.remove_qty_of_product("Ancient Beast", 5)
+	  @building_stub.remove_qty_of_product("Ancient Beast", 5)
 	end
 	assert_false(@was_notified_of_change)
 	
-	@building_mock.delete_observer(self)
+	@building_stub.delete_observer(self)
   end
   
   def test_building_that_is_not_observable_can_still_add_and_remove_products_without_error
-	@building_mock = NotObservableUnrestrictedStorageMock.new
+	@building_stub = NotObservableUnrestrictedStorageStub.new
 	
-	assert_equal(0.0, @building_mock.volume_used)
+	assert_equal(0.0, @building_stub.volume_used)
 	
-	@building_mock.store_product("Dwarf", 1)
-	assert_equal(0.01, @building_mock.volume_used)
+	@building_stub.store_product("Dwarf", 1)
+	assert_equal(0.01, @building_stub.volume_used)
 	
-	@building_mock.store_product("Ancient Beast", 1)
-	assert_equal(0.39, @building_mock.volume_used)
+	@building_stub.store_product("Ancient Beast", 1)
+	assert_equal(0.39, @building_stub.volume_used)
 	
-	@building_mock.remove_all_of_product("Dwarf")
-	assert_equal(0.38, @building_mock.volume_used)
+	@building_stub.remove_all_of_product("Dwarf")
+	assert_equal(0.38, @building_stub.volume_used)
 	
-	@building_mock.remove_qty_of_product("Ancient Beast", 1)
-	assert_equal(0.0, @building_mock.volume_used)
+	@building_stub.remove_qty_of_product("Ancient Beast", 1)
+	assert_equal(0.0, @building_stub.volume_used)
   end
 end
