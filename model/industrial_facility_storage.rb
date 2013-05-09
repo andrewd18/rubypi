c34771f3f6ed58607aaae81c9bc4128ceb2ca68c
Andrew Dorney <andrewd18@gmail.com>
2013-05-09 14:29:14 -0500

    Add test cases for IndustrialFacilityStorage module.

--- model/industrial_facility_storage.rb
+++ model/industrial_facility_storage.rb
@@ -3,10 +3,82 @@
 # Expects the factory class
 # 
 # Including class must:
-#  * 
+#  * call self.industrial_facility_storage_schematic_changed whenever the schematic changes.
 
 require_relative 'product.rb'
 require_relative 'schematic.rb'
 
 module IndustrialFacilityStorage
+  def stored_products
+	return @stored_products ||= Hash.new
+  end
+  
+  def stored_products=(new_stored_products)
+	raise ArgumentError, "Argument is not a Hash." unless new_stored_products.is_a?(Hash)
+	
+	@stored_products = new_stored_products
+  end
+  
+  def industrial_facility_storage_schematic_changed
+	if (self.schematic.nil?)
+	  # Erase product categories and quantities entirely.
+	  stored_products.clear
+	else
+	  # Erase product categories and quantities entirely.
+	  stored_products.clear
+	  
+	  # Get the new storage mins and max quantities.
+	  self.schematic.inputs.each_pair do |product_name, max_storable_quantity|
+		# Assign to product_name with zero stored by default.
+		stored_products[product_name] = 0
+	  end
+	end
+  end
+  
+  def store_product(product_name, quantity)
+	raise ArgumentError, "Schematic is not set." unless (self.schematic != nil)
+	raise ArgumentError, "Named product is not part of the set Schematic." unless self.schematic.inputs.has_key?(product_name)
+	
+	# We have a schematic and the person is adding a product from it.
+	# Check if this would overflow.
+	max_allowed_storage_for_this_product = self.schematic.inputs[product_name]
+	currently_stored_quantity = stored_products[product_name]
+	
+	# If it would overflow, get upset.
+	if (quantity + currently_stored_quantity > max_allowed_storage_for_this_product)
+	  raise ArgumentError, "Adding this much #{product_name} would overflow the storage area."
+	else
+	  # Add to the existing column.
+	  stored_products[product_name] += quantity
+	end
+  end
+  
+  def remove_all_stored_products
+	stored_products.each_key do |product_name|
+	  remove_all_of_product(product_name)
+	end
+  end
+  
+  def remove_all_of_product(product_name)
+	raise ArgumentError, "Schematic is not set." unless (self.schematic != nil)
+	raise ArgumentError, "Named product is not part of the set Schematic." unless self.schematic.inputs.has_key?(product_name)
+	
+	stored_products[product_name] = 0
+  end
+  
+  def remove_qty_of_product(product_name, quantity)
+	raise ArgumentError, "Schematic is not set." unless (self.schematic != nil)
+	raise ArgumentError, "Named product is not part of the set Schematic." unless self.schematic.inputs.has_key?(product_name)
+	raise ArgumentError, "Named product is not stored here." unless self.stored_products.has_key?(product_name)
+	
+	currently_stored_quantity = stored_products[product_name]
+	
+	# If it would underflow, set it to zero.
+	if (currently_stored_quantity - quantity < 0)
+	  stored_products[product_name] = 0
+	else
+	  # Remove it from the column.
+	  stored_products[product_name] -= quantity
+	end
+  end
 end
--- tests/test_case_industrial_facility_storage.rb
+++ tests/test_case_industrial_facility_storage.rb
@@ -6,8 +6,13 @@ class IndustrialFacilityStorageStub
   include Observable
   include IndustrialFacilityStorage
   
-  # TODO
-  # Add required methods here.
+  attr_reader :schematic
+  
+  def schematic=(new_schematic)
+	@schematic = new_schematic
+	
+	self.industrial_facility_storage_schematic_changed
+  end
 end
 
 class TestCaseIndustrialFacilityStorage < Test::Unit::TestCase
@@ -16,6 +21,16 @@ class TestCaseIndustrialFacilityStorage < Test::Unit::TestCase
 	@@chiral_structures = Product.find_or_create("Chiral Structures", 0)
 	@@silicon = Product.find_or_create("Silicon", 0)
 	@@miniature_electronics = Product.find_or_create("Miniature Electronics", 1)
+	
+	@@miniature_electronics_schematic = Schematic.new("Miniature Electronics", 5, {"Chiral Structures" => 40, "Silicon" => 40})
+	
+	@@mechanical_parts = Product.find_or_create("Mechanical Parts", 1)
+	@@supertensile_plastics = Product.find_or_create("Supertensile Plastics", 1)
+	# @@miniature_electronics already created
+	
+	@@planetary_vehicles = Product.find_or_create("Planetary Vehicles", 2)
+	
+	@@planetary_vehicles_schematic = Schematic.new("Planetary Vehicles", 3, {"Supertensile Plastics" => 10, "Mechanical Parts" => 10, "Miniature Electronics" => 10})
   end
   
   # Run once after all tests.
@@ -23,6 +38,8 @@ class TestCaseIndustrialFacilityStorage < Test::Unit::TestCase
 	Product.delete(@@chiral_structures)
 	Product.delete(@@silicon)
 	Product.delete(@@miniature_electronics)
+	
+	Schematic.delete(@@miniature_electronics_schematic)
   end
 
   # Run before every test.
@@ -36,114 +53,224 @@ class TestCaseIndustrialFacilityStorage < Test::Unit::TestCase
   end
   
   def test_industrial_facility_storage_schematic_changed_when_schematic_changes_from_nil_to_a_valid_schematic
-	pend
-	# method: industrial_facility_storage_schematic_changed
+	assert_equal(nil, @building_stub.schematic)
+	@building_stub.schematic = @@miniature_electronics_schematic
+	assert_equal(@@miniature_electronics_schematic, @building_stub.schematic)
 	
-	# New, empty storages should be created.
-	# The number of storages created should be equal to the number of inputs on the schematic.
-	# Storages should only hold schematic.inputquantity amount of items.
+	expected_storage_hash = {"Chiral Structures" => 0, "Silicon" => 0}
+	
+	assert_equal(expected_storage_hash, @building_stub.stored_products)
   end
   
   def test_industrial_facility_storage_schematic_changed_when_schematic_changes_from_one_valid_schematic_to_another
-	pend
-	# method: industrial_facility_storage_schematic_changed
+	# Repeat what we just did in the from_nil_to_valid test.
+	@building_stub.schematic = @@miniature_electronics_schematic
+	assert_equal(@@miniature_electronics_schematic, @building_stub.schematic)
 	
-	# All stored products should be deleted.
-	# All existing storages should be deleted.
-	# New, empty storages should be created.
-	# The number of storages created should be equal to the number of inputs on the schematic.
-	# Storages should only hold schematic.inputquantity amount of items.
+	# Now set it to something else.
+	@building_stub.schematic = @@planetary_vehicles_schematic
+	assert_equal(@@planetary_vehicles_schematic, @building_stub.schematic)
+	
+	expected_storage_hash = {"Supertensile Plastics" => 0, "Mechanical Parts" => 0, "Miniature Electronics" => 0}
+	
+	assert_equal(expected_storage_hash, @building_stub.stored_products)
   end
   
   def test_industrial_facility_storage_schematic_changed_when_schematic_changes_from_a_valid_schematic_to_nil
-	pend
-	# method: industrial_facility_storage_schematic_changed
+	# Repeat what we just did in the from_nil_to_valid test.
+	@building_stub.schematic = @@miniature_electronics_schematic
+	assert_equal(@@miniature_electronics_schematic, @building_stub.schematic)
 	
-	# All stored products should be deleted.
-	# No new storages should be created.
+	# Now set it to nil.
+	@building_stub.schematic = nil
+	assert_equal(nil, @building_stub.schematic)
+	
+	empty_storage_hash = {}
+	
+	assert_equal(empty_storage_hash, @building_stub.stored_products)
   end
   
   def test_store_product_should_error_when_schematic_name_is_nil
-	pend
-	# No change should occur in the stored products.
+	empty_storage_hash = {}
+	
+	assert_equal(nil, @building_stub.schematic)
+	assert_equal(empty_storage_hash, @building_stub.stored_products)
+	
+	# Test adding 5 silicon.
+	assert_raise ArgumentError do
+	  @building_stub.store_product("Silicon", 5)
+	end
+	
+	# Make sure nothing changed.
+	assert_equal(nil, @building_stub.schematic)
+	assert_equal(empty_storage_hash, @building_stub.stored_products)
   end
   
   def test_store_product_should_store_product_in_appropriate_storage
-	pend
-	# Start with empty but valid storages.
-	# Add a product.
-	# Make sure the right bucket got the product and quantity.
-	# Make sure the wrong buckets are still empty.
+	# Repeat what we just did in the from_nil_to_valid test.
+	@building_stub.schematic = @@miniature_electronics_schematic
+	assert_equal(@@miniature_electronics_schematic, @building_stub.schematic)
+	
+	@building_stub.store_product("Silicon", 5)
+	
+	expected_storage_hash = {"Chiral Structures" => 0, "Silicon" => 5}
+	
+	assert_equal(expected_storage_hash, @building_stub.stored_products)
   end
   
   def test_store_product_should_error_when_given_product_is_not_an_input_on_the_schematic
-	pend
-	# Start with empty but valid storages.
-	# Add a product that is completely unrelated.
-	# Make sure we error.
-	# Make sure the buckets are still empty.
+	# Repeat what we just did in the from_nil_to_valid test.
+	@building_stub.schematic = @@planetary_vehicles_schematic
+	assert_equal(@@planetary_vehicles_schematic, @building_stub.schematic)
+	
+	assert_raise ArgumentError do
+	  @building_stub.store_product("Silicon", 5)
+	end
+	
+	expected_storage_hash = {"Supertensile Plastics" => 0, "Mechanical Parts" => 0, "Miniature Electronics" => 0}
+	
+	assert_equal(expected_storage_hash, @building_stub.stored_products)
   end
   
   def test_store_product_should_error_when_given_product_amount_would_overflow_storage
-	pend
-	# Start with empty but valid storages.
-	# Add wayyyy too much of a valid product.
-	# Make sure we error.
-	# Make sure the buckets are still empty.
+	# Repeat what we just did in the from_nil_to_valid test.
+	@building_stub.schematic = @@miniature_electronics_schematic
+	assert_equal(@@miniature_electronics_schematic, @building_stub.schematic)
+	
+	assert_raise ArgumentError do
+	  @building_stub.store_product("Silicon", 9999)
+	end
+	
+	empty_storage_hash = {"Chiral Structures" => 0, "Silicon" => 0}
+	
+	assert_equal(empty_storage_hash, @building_stub.stored_products)
+	
+	
+	# Now attempt to overflow by one.
+	# Set to max.
+	@building_stub.store_product("Silicon", 40)
+	
+	assert_raise ArgumentError do
+	  @building_stub.store_product("Silicon", 1)
+	end
+	
+	expected_storage_hash = {"Chiral Structures" => 0, "Silicon" => 40}
+	
+	assert_equal(expected_storage_hash, @building_stub.stored_products)
   end
   
   def test_remove_all_of_product_should_error_when_schematic_name_is_nil
-	pend
-	# No change should occur in the stored products.
+	empty_storage_hash = {}
+	
+	assert_equal(empty_storage_hash, @building_stub.stored_products)
+	
+	assert_raise ArgumentError do
+	  @building_stub.remove_all_of_product("Mechanical Parts")
+	end
+	
+	assert_equal(empty_storage_hash, @building_stub.stored_products)
   end
   
   def test_remove_all_of_product_should_remove_product_from_appropriate_storage
-	pend
-	# Start with empty but valid storages.
-	# Fill each storage to brim.
-	# Remove all of one.
-	# Make sure the right bucket is empty.
-	# Make sure the wrong buckets are still full.
+	@building_stub.schematic = @@miniature_electronics_schematic
+	
+	# Fill the storages up.
+	@building_stub.store_product("Chiral Structures", 40)
+	@building_stub.store_product("Silicon", 40)
+	
+	full_storage = {"Chiral Structures" => 40, "Silicon" => 40}
+	storage_with_silicon_empty = {"Chiral Structures" => 40, "Silicon" => 0}
+	
+	assert_equal(full_storage, @building_stub.stored_products)
+	
+	@building_stub.remove_all_of_product("Silicon")
+	
+	assert_equal(storage_with_silicon_empty, @building_stub.stored_products)
   end
   
   def test_remove_all_of_product_should_error_when_given_product_is_not_an_input_on_the_schematic
-	pend
-	# Start with empty but valid storages.
-	# Fill each storage to brim.
-	# Remove all of something completely unrelated.
-	# Make sure we error.
-	# Make sure the buckets are still full.
+	@building_stub.schematic = @@miniature_electronics_schematic
+	
+	# Fill the storages up.
+	@building_stub.store_product("Chiral Structures", 40)
+	@building_stub.store_product("Silicon", 40)
+	
+	full_storage = {"Chiral Structures" => 40, "Silicon" => 40}
+	
+	assert_equal(full_storage, @building_stub.stored_products)
+	
+	assert_raise ArgumentError do
+	  @building_stub.remove_all_of_product("Mechanical Parts")
+	end
+	
+	assert_equal(full_storage, @building_stub.stored_products)
   end
   
   def test_remove_qty_of_product_should_error_when_schematic_name_is_nil
-	pend
-	# No change should occur in the stored products.
+	empty_storage_hash = {}
+	
+	assert_equal(nil, @building_stub.schematic)
+	assert_equal(empty_storage_hash, @building_stub.stored_products)
+	
+	# Test removing 5 silicon.
+	assert_raise ArgumentError do
+	  @building_stub.remove_qty_of_product("Silicon", 5)
+	end
+	
+	# Make sure nothing changed.
+	assert_equal(nil, @building_stub.schematic)
+	assert_equal(empty_storage_hash, @building_stub.stored_products)
   end
   
   def test_remove_qty_of_product_should_remove_product_from_appropriate_storage
-	pend
-	# Start with empty but valid storages.
-	# Fill each storage to brim.
-	# Remove half of one.
-	# Make sure the right bucket is at half.
-	# Make sure the wrong buckets are still full.
+	@building_stub.schematic = @@miniature_electronics_schematic
+	
+	# Fill the storages up.
+	@building_stub.store_product("Chiral Structures", 40)
+	@building_stub.store_product("Silicon", 40)
+	
+	full_storage = {"Chiral Structures" => 40, "Silicon" => 40}
+	storage_with_silicon_half_empty = {"Chiral Structures" => 40, "Silicon" => 20}
+	
+	assert_equal(full_storage, @building_stub.stored_products)
+	
+	@building_stub.remove_qty_of_product("Silicon", 20)
+	
+	assert_equal(storage_with_silicon_half_empty, @building_stub.stored_products)
   end
   
   def test_remove_qty_of_product_should_error_when_given_product_is_not_an_input_on_the_schematic
-	pend
-	# Start with empty but valid storages.
-	# Fill each storage to brim.
-	# Remove quantity of something completely unrelated.
-	# Make sure we error.
-	# Make sure the buckets are still full.
+	@building_stub.schematic = @@miniature_electronics_schematic
+	
+	# Fill the storages up.
+	@building_stub.store_product("Chiral Structures", 40)
+	@building_stub.store_product("Silicon", 40)
+	
+	full_storage = {"Chiral Structures" => 40, "Silicon" => 40}
+	
+	assert_equal(full_storage, @building_stub.stored_products)
+	
+	assert_raise ArgumentError do
+	  @building_stub.remove_qty_of_product("Mechanical Parts", 20)
+	end
+	
+	assert_equal(full_storage, @building_stub.stored_products)
   end
   
   def test_remove_qty_of_product_should_remove_all_of_product_from_appropriate_storage_if_remove_qty_is_more_than_stored_qty
-	pend
-	# Start with empty but valid storages.
-	# Fill each storage to brim.
-	# Remove double of one.
-	# Make sure the right bucket is empty and not a negative value.
-	# Make sure the other buckets are still full.
+	@building_stub.schematic = @@miniature_electronics_schematic
+	
+	# Fill the storages up.
+	@building_stub.store_product("Chiral Structures", 40)
+	@building_stub.store_product("Silicon", 40)
+	
+	full_storage = {"Chiral Structures" => 40, "Silicon" => 40}
+	storage_with_silicon_empty = {"Chiral Structures" => 40, "Silicon" => 00}
+	
+	assert_equal(full_storage, @building_stub.stored_products)
+	
+	@building_stub.remove_qty_of_product("Silicon", 999999999)
+	
+	assert_equal(storage_with_silicon_empty, @building_stub.stored_products)
   end
 end
