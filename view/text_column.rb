aaf58eb2e4435d39b59082a52ba6b4d54b778628
Andrew Dorney <andrewd18@gmail.com>
2013-05-09 16:16:55 -0500

    Add word wrapping to the buildings table.

--- view/text_column.rb
+++ view/text_column.rb
@@ -2,6 +2,11 @@ class TextColumn < Gtk::TreeViewColumn
   def initialize(name, model_column)
 	text_renderer = Gtk::CellRendererText.new
 	
+	# Attempt to word wrap after wrap_width characters.
+	# Only wrap at word boundaries.
+	text_renderer.wrap_width = 20
+	text_renderer.wrap_mode = Pango::WRAP_WORD
+	
 	super(name, text_renderer, :text => model_column)
 	
 	self.reorderable = true
