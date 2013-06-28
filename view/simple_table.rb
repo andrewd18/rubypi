1f788b0a9012c9d1dce0804da1157b3cb6a28f49
Andrew Dorney <andrewd18@gmail.com>
2013-06-28 16:32:43 -0500

    Allow simple_table to override width and height options into table super.

--- view/simple_table.rb
+++ view/simple_table.rb
@@ -6,7 +6,7 @@ class SimpleTable < Gtk::Table
   # Keep the standard initialize method.
   
   # Override attach
-  def attach(widget, row, column)
+  def attach(widget, row, column, expand_width = true, fill_width = true, expand_height = true, fill_height = true)
 	raise ArgumentError unless (row.is_a?(Numeric))
 	raise ArgumentError unless (column.is_a?(Numeric))
 	
@@ -16,7 +16,41 @@ class SimpleTable < Gtk::Table
 	column_start = (column - 1)
 	column_end = column
 	
+	
+	if ((expand_width == true) and
+	    (fill_width == true))
+	  
+	  width_options = [Gtk::AttachOptions::EXPAND, Gtk::AttachOptions::FILL]
+	  
+	elsif ((expand_width == true) and
+	       (fill_width == false))
+	  
+	  width_options = [Gtk::AttachOptions::EXPAND]
+	  
+	else
+	  
+	  width_options = []
+	end
+	
+	
+	if ((expand_height == true) and
+	    (fill_height == true))
+	  
+	  height_options = [Gtk::AttachOptions::EXPAND, Gtk::AttachOptions::FILL]
+	  
+	elsif ((expand_height == true) and
+	       (expand_width == false))
+	  
+	  height_options = [Gtk::AttachOptions::EXPAND]
+	  
+	else
+	  
+	  height_options = []
+	end
+	
+	
+	
 	# Call the "real" attach with the determined options.
-	super(widget, column_start, column_end, row_start, row_end)
+	super(widget, column_start, column_end, row_start, row_end, width_options, height_options)
   end
 end
