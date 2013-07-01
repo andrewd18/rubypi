98940ec0cf0f726c375f255ceeb3f22414c2d39d
Andrew Dorney <andrewd18@gmail.com>
2013-07-01 17:05:06 -0500

    Remove deprecated code.

--- view/transfer_products_dialog.rb
+++ view/transfer_products_dialog.rb
@@ -3,7 +3,6 @@ require_relative 'select_building_combo_box.rb'
 require_relative 'simple_table.rb'
 
 class TransferProductsDialog < Gtk::Dialog
-  attr_reader :hash_to_transfer
   
   def initialize(planet_model, source_building = nil)
 	title = "Transfer Products"
@@ -13,7 +12,6 @@ class TransferProductsDialog < Gtk::Dialog
 	second_button_response_id_combo = [Gtk::Stock::CANCEL, Gtk::ResponseType::REJECT]
 	
 	@planet_model = planet_model
-	@hash_to_transfer = Hash.new
 	
 	super(:title => title, :parent => parent_window, :flags => flags, :buttons => [first_button_response_id_combo, second_button_response_id_combo])
 	
@@ -57,20 +55,19 @@ class TransferProductsDialog < Gtk::Dialog
 	
 	# Layout (needed for #update)
 	                            # rows, columns, homogenous?
-	layout_table = SimpleTable.new(3, 3, false)
+	layout_table = SimpleTable.new(3, 2, false)
 	
 	# Pack all the things that haven't been dynamically created.
 	
 	# First row
 	layout_table.attach(source_label, 1, 1)
-	layout_table.attach(products_to_transfer_label, 1, 2)
-	layout_table.attach(destination_label, 1, 3)
+	layout_table.attach(destination_label, 1, 2)
 	
 	
 	# Second row
 	layout_table.attach(@source_combo_box, 2, 1)
 	# blank space
-	layout_table.attach(@destination_combo_box, 2, 3)
+	layout_table.attach(@destination_combo_box, 2, 2)
 	
 	
 	# Third row.
@@ -85,11 +82,6 @@ class TransferProductsDialog < Gtk::Dialog
 	source_stored_products_frame.add(source_stored_products_vbox)
 	layout_table.attach(source_stored_products_frame, 3, 1)
 	
-	# Center column.
-	# This gets filled in by add_product_to_transfer_list.
-	@transfer_products_vbox = Gtk::Box.new(:vertical)
-	layout_table.attach(@transfer_products_vbox, 3, 2)
-	
 	# Right column.
 	destination_stored_products_scrollbox.add(@destination_stored_products_tree_view)
 	
@@ -99,7 +91,7 @@ class TransferProductsDialog < Gtk::Dialog
 	
 	destination_stored_products_frame = Gtk::Frame.new
 	destination_stored_products_frame.add(destination_stored_products_vbox)
-	layout_table.attach(destination_stored_products_frame, 3, 3)
+	layout_table.attach(destination_stored_products_frame, 3, 2)
 	
 	self.child.add(layout_table)
 	
@@ -116,32 +108,6 @@ class TransferProductsDialog < Gtk::Dialog
   def destination_changed
 	# Set the destination_stored_products_table to the building model pointed at by @destination_combo_box
 	@destination_stored_products_tree_view.building_model = self.destination
-  end
-  
-  def add_product_to_transfer_list
-	# Figure out what the user clicked on.
-	row = @source_stored_products_list_view.selection
-	tree_iter = row.selected
-	selected_product_name = tree_iter.get_value(1)
-	
-	source_building = @source_combo_box.selected_item
-	currently_stored_quantity = source_building.stored_products[selected_product_name]
-	
-	# Now we know what the user clicked on, as well as the quantity stored in the source building.
-	# Add a new label and slider.
-	product_name_label = Gtk::Label.new("#{selected_product_name}")
-	product_quantity_slider = Gtk::Scale.new(:horizontal, 0, currently_stored_quantity, 1)
-	product_quantity_slider.signal_connect("value-changed") do |slider|
-	  @hash_to_transfer[selected_product_name]= slider.value
-	end
-	
-	product_row = Gtk::Box.new(:horizontal)
-	product_row.pack_start(product_name_label, :expand => false)
-	product_row.pack_start(product_quantity_slider, :expand => true)
-	
-	@transfer_products_vbox.pack_start(product_row, :expand => false)
-	
-	@transfer_products_vbox.show_all
   end
   
   def source
