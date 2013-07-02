f2cf02f38d3ce0858d753d353c629550e31a9156
Andrew Dorney <andrewd18@gmail.com>
2013-07-02 16:18:05 -0500

    Make add_products_select_quantity_dialog act more like transfer_products dialog.

--- view/add_products_select_quantity_dialog.rb
+++ view/add_products_select_quantity_dialog.rb
@@ -4,26 +4,29 @@ class AddProductsSelectQuantityDialog < Gtk::Dialog
   attr_reader :quantity
   
   def initialize(building_model, product_name, parent_window = nil)
-	title = "Add Products to Building"
+	title = "Add #{product_name} to #{building_model.name}"
 	flags = Gtk::Dialog::Flags::MODAL
 	first_button_response_id_combo = [Gtk::Stock::OK, Gtk::ResponseType::ACCEPT]
 	second_button_response_id_combo = [Gtk::Stock::CANCEL, Gtk::ResponseType::REJECT]
 	
-	@building_model = building_model
-	
 	super(:title => title, :parent => parent_window, :flags => flags, :buttons => [first_button_response_id_combo, second_button_response_id_combo])
 	
 	product_instance = Product.find_by_name(product_name)
-	most_amount_addable = (@building_model.volume_available / product_instance.volume)
+	most_amount_addable = (building_model.volume_available / product_instance.volume)
 	# Force an int.
 	most_amount_addable = most_amount_addable.to_i
 	
-	# Fill top of dialog in.
-	how_many_label = Gtk::Label.new("How Many?")
+	product_name_label = Gtk::Label.new("#{product_name}")
 	@product_quantity_slider = Gtk::Scale.new(:horizontal, 0, most_amount_addable, 1)
 	
-	self.child.add(how_many_label)
-	self.child.add(@product_quantity_slider)
+	hbox = Gtk::Box.new(:horizontal)
+	hbox.pack_start(product_name_label, :expand => false)
+	hbox.pack_start(@product_quantity_slider, :expand => true)
+	
+	# WORKAROUND
+	# "self.child" is actually a vbox which we connect things to via pack_start.
+	# For some reason self.vbox is deprecated. :/
+	self.child.pack_start(hbox, :expand => false)
 	
 	self.show_all
 	
