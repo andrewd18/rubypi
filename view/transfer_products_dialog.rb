c3f9b2336cdd263832794041ae0b69b012a382a5
Andrew Dorney <andrewd18@gmail.com>
2013-07-02 09:34:00 -0500

    Fix issue where Transfer Products Dialog would not scale to large windows properly. Fix some minor naming convention bugs.

--- view/transfer_products_dialog.rb
+++ view/transfer_products_dialog.rb
@@ -17,7 +17,6 @@ class TransferProductsDialog < Gtk::Dialog
 	
 	# Create all the widgets we'll be using.
 	source_label = Gtk::Label.new("Source:")
-	products_to_transfer_label = Gtk::Label.new("Products to Transfer:")
 	destination_label = Gtk::Label.new("Destination:")
 	
 	@source_combo_box = SelectBuildingComboBox.new(@planet_model.aggregate_launchpads_ccs_storages)
@@ -33,8 +32,8 @@ class TransferProductsDialog < Gtk::Dialog
 	
 	
 	source_stored_products_label = Gtk::Label.new("Stored Products")
-	@source_stored_products_list_view = StoredProductsTreeView.new
-	@source_stored_products_list_view.signal_connect("row-activated") do
+	@source_stored_products_tree_view = StoredProductsTreeView.new
+	@source_stored_products_tree_view.signal_connect("row-activated") do
 	  self.create_how_many_dialog
 	end
 	
@@ -51,48 +50,58 @@ class TransferProductsDialog < Gtk::Dialog
 	# Never have a horizontal scrollbar. Have a vertical scrollbar if necessary.
 	destination_stored_products_scrollbox.set_policy(Gtk::PolicyType::NEVER, Gtk::PolicyType::AUTOMATIC)
 	
+	# WORKAROUND
+	# "self.child" is actually a vbox which we connect things to via pack_start.
+	# For some reason self.vbox is deprecated. :/
 	
-	# Layout (needed for #update)
-	                            # rows, columns, homogenous?
-	layout_table = SimpleTable.new(3, 2, false)
+	                          #rows, columns, homogenous?
+	layout_table = SimpleTable.new(2, 2, false)
 	
-	# Pack all the things that haven't been dynamically created.
+	# First row.
+	# First cell
+	source_selection_vbox = Gtk::Box.new(:vertical)
+	source_selection_vbox.pack_start(source_label, :expand => false)
+	source_selection_vbox.pack_start(@source_combo_box, :expand => false)
 	
-	# First row
-	layout_table.attach(source_label, 1, 1)
-	layout_table.attach(destination_label, 1, 2)
-	
-	
-	# Second row
-	layout_table.attach(@source_combo_box, 2, 1)
-	# blank space
-	layout_table.attach(@destination_combo_box, 2, 2)
+	# Wrap it in a frame and pack it in the layout table.
+	source_label_and_combo_box_frame = Gtk::Frame.new
+	source_label_and_combo_box_frame.add(source_selection_vbox)
+	layout_table.attach(source_label_and_combo_box_frame, 1, 1, true, true, false, false)
 	
 	
-	# Third row.
-	# Left column.
-	source_stored_products_scrollbox.add(@source_stored_products_list_view)
+	# Second cell.
+	destination_selection_vbox = Gtk::Box.new(:vertical)
+	destination_selection_vbox.pack_start(destination_label, :expand => false)
+	destination_selection_vbox.pack_start(@destination_combo_box, :expand => false)
 	
+	# Wrap it in a frame and pack it in the layout table.
+	destination_label_and_combo_box_frame = Gtk::Frame.new
+	destination_label_and_combo_box_frame.add(destination_selection_vbox)
+	layout_table.attach(destination_label_and_combo_box_frame, 1, 2, true, true, false, false)
+	
+	
+	# Second row.
+	# First cell.
+	source_stored_products_scrollbox.add(@source_stored_products_tree_view)
 	source_stored_products_vbox = Gtk::Box.new(:vertical)
 	source_stored_products_vbox.pack_start(source_stored_products_label, :expand => false)
 	source_stored_products_vbox.pack_start(source_stored_products_scrollbox, :expand => true)
 	
 	source_stored_products_frame = Gtk::Frame.new
 	source_stored_products_frame.add(source_stored_products_vbox)
-	layout_table.attach(source_stored_products_frame, 3, 1)
+	layout_table.attach(source_stored_products_frame, 2, 1)
 	
-	# Right column.
+	# Second cell
 	destination_stored_products_scrollbox.add(@destination_stored_products_tree_view)
-	
 	destination_stored_products_vbox = Gtk::Box.new(:vertical)
 	destination_stored_products_vbox.pack_start(destination_stored_products_label, :expand => false)
 	destination_stored_products_vbox.pack_start(destination_stored_products_scrollbox, :expand => true)
 	
 	destination_stored_products_frame = Gtk::Frame.new
 	destination_stored_products_frame.add(destination_stored_products_vbox)
-	layout_table.attach(destination_stored_products_frame, 3, 2)
+	layout_table.attach(destination_stored_products_frame, 2, 2)
 	
-	self.child.add(layout_table)
+	self.child.pack_start(layout_table, :expand => true)
 	
 	self.show_all
 	
@@ -101,7 +110,7 @@ class TransferProductsDialog < Gtk::Dialog
   
   def source_changed
 	# Set the source_stored_products_table to the building model pointed at by @source_combo_box
-	@source_stored_products_list_view.building_model = self.source
+	@source_stored_products_tree_view.building_model = self.source
   end
   
   def destination_changed
@@ -122,7 +131,7 @@ class TransferProductsDialog < Gtk::Dialog
 	return unless (self.destination != nil)
 	
 	# Figure out what the user clicked on.
-	row = @source_stored_products_list_view.selection
+	row = @source_stored_products_tree_view.selection
 	tree_iter = row.selected
 	
 	selected_product_name = tree_iter.get_value(1)
