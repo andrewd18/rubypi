fa78c5a6b64c7a9e7c4ed2b05489010803bbcdb9
Andrew Dorney <andrewd18@gmail.com>
2013-08-12 14:41:18 -0500

    Cleanup comments.

--- view/planet_view/building_layout_widget.rb
+++ view/planet_view/building_layout_widget.rb
@@ -3,29 +3,14 @@ require 'gtk3'
 require_relative 'building_tool_palette.rb'
 require_relative 'building_drawing_area.rb'
 
-
-
-# CREATE
-# On-click, adds selected building type to model at location.
-# On-hover, shows selected building type under cursor, faded out.
-# On-hover, shows selected building type under cursor, red-shaded if coordinates taken.
-
-# READ
-# Draws each building
-# Draws each link
-
-# UPDATE / EDIT
-# On model change, update all.
-
-# DESTROY
-# On <something> remove building and connected links from the model.
-
 class BuildingLayoutWidget < Gtk::Frame
   def initialize(controller)
+	# Create a Gtk::Frame.
 	super()
 	
 	@controller = controller
 	
+	# Create a drawing area and a tool palette linked to it.
 	@building_drawing_area = BuildingDrawingArea.new(@controller)
 	@drawing_tool_palette = BuildingToolPalette.new(@building_drawing_area)
 	
@@ -40,6 +25,7 @@ class BuildingLayoutWidget < Gtk::Frame
   end
   
   def planet_model=(new_planet_model)
+	# Pass the new model to the drawing area.
 	@building_drawing_area.planet_model = new_planet_model
   end
 end
