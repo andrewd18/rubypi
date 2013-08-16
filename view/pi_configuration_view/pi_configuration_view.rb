869467ec7f0e5adb52dfa9d904e3d207cc6998be
Andrew Dorney <andrewd18@gmail.com>
2013-08-16 10:29:10 -0500

    Add a PI Configuration View header. Move the open and save buttons up to the header. Add more framing to the pi_config_view widgets.

--- view/pi_configuration_view/pi_configuration_view.rb
+++ view/pi_configuration_view/pi_configuration_view.rb
@@ -8,24 +8,13 @@ require_relative 'load_from_yaml_dialog.rb'
 class PIConfigurationView < Gtk::Box
   def initialize(controller)
 	# Set up base GTK+ requirements.
-	super(:horizontal)
+	super(:vertical)
 	
 	# Store the controller whose actions this view can use.
 	@controller = controller
 	
-	# Set up all the widgets unique to this view. Delegate to subclasses where necessary.
-	@planet_tool_palette = PlanetToolPalette.new(@controller)
-	@planet_list = PlanetList.new(@controller)
-	
-	planet_list_scrolled_window = Gtk::ScrolledWindow.new
-	planet_list_scrolled_window.set_policy(Gtk::PolicyType::NEVER, Gtk::PolicyType::AUTOMATIC)
-	planet_list_scrolled_window.add(@planet_list)
-	
-	
-	edit_selected_button = Gtk::Button.new(:stock_id => Gtk::Stock::EDIT)
-	edit_selected_button.signal_connect("clicked") do |button|
-	  @controller.edit_selected_planet(@planet_list.selected_planet_instance)
-	end
+	# Description and up button row widgets.
+	description_label = Gtk::Label.new("PI Configuration View")
 	
 	import_from_yaml_button = Gtk::Button.new(:stock_id => Gtk::Stock::OPEN)
 	import_from_yaml_button.signal_connect("clicked") do |button|
@@ -37,15 +26,65 @@ class PIConfigurationView < Gtk::Box
 	  self.save_to_yaml
 	end
 	
-	button_box = Gtk::Box.new(:vertical)
-	button_box.pack_start(edit_selected_button, :expand => false)
-	button_box.pack_start(import_from_yaml_button, :expand => false)
-	button_box.pack_start(export_to_yaml_button, :expand => false)
+	# Pack the description and up button row widgets.
+	top_row = Gtk::Box.new(:horizontal)
+	top_row.pack_start(description_label, :expand => true)
+	top_row.pack_start(import_from_yaml_button, :expand => false)
+	top_row.pack_start(export_to_yaml_button, :expand => false)
+	self.pack_start(top_row, :expand => false)
 	
 	
-	self.pack_start(@planet_tool_palette, :expand => false)
-	self.pack_start(planet_list_scrolled_window, :expand => true)
-	self.pack_start(button_box, :expand => false, :padding => 10)
+	# Create the Bottom Row
+	bottom_row = Gtk::Box.new(:horizontal)
+	
+	# Set up all the widgets unique to this view. Delegate to subclasses where necessary.
+	# Left column.
+	left_column_frame = Gtk::Frame.new
+	left_column_vbox = Gtk::Box.new(:vertical)
+	@planet_tool_palette = PlanetToolPalette.new(@controller)
+	left_column_vbox.pack_start(@planet_tool_palette, :expand => false)
+	left_column_frame.add(left_column_vbox)
+	
+	
+	# Center column.
+	center_column_frame = Gtk::Frame.new
+	center_column = Gtk::Box.new(:vertical)
+	
+	planet_list_scrolled_window = Gtk::ScrolledWindow.new
+	planet_list_scrolled_window.set_policy(Gtk::PolicyType::AUTOMATIC, Gtk::PolicyType::AUTOMATIC)
+	
+	@planet_list = PlanetList.new(@controller)
+	planet_list_scrolled_window.add(@planet_list)
+	
+	
+	button_box = Gtk::Box.new(:horizontal)
+	edit_selected_button = Gtk::Button.new(:stock_id => Gtk::Stock::EDIT)
+	edit_selected_button.signal_connect("clicked") do |button|
+	  unless (@planet_list.selected_planet_instance == nil)
+		@controller.edit_selected_planet(@planet_list.selected_planet_instance)
+	  end
+	end
+	button_box.pack_end(edit_selected_button, :expand => false)
+	
+	center_column.pack_start(planet_list_scrolled_window, :expand => true)
+	center_column.pack_start(button_box, :expand => false)
+	center_column_frame.add(center_column)
+	
+	# Right column.
+	right_column_frame = Gtk::Frame.new
+	right_column = Gtk::Box.new(:vertical)
+	eve_gate_image = Gtk::Image.new(:file => "view/images/eve_gate_128x128.png")
+	@num_colonized_planets_label = Gtk::Label.new("Colonized Planets: 0")
+	
+	right_column.pack_start(eve_gate_image, :expand => false)
+	right_column.pack_start(@num_colonized_planets_label, :expand => false)
+	right_column_frame.add(right_column)
+	
+	bottom_row.pack_start(left_column_frame, :expand => false)
+	bottom_row.pack_start(center_column_frame, :expand => true, :fill => true)
+	bottom_row.pack_start(right_column_frame, :expand => false)
+	
+	self.pack_start(bottom_row, :expand => true)
 	
 	self.show_all
 	
@@ -85,5 +124,7 @@ class PIConfigurationView < Gtk::Box
   def pi_configuration_model=(new_model)
 	# Update the view using the new model for values.
 	@planet_list.pi_configuration_model = new_model
+	
+	@num_colonized_planets_label.text = "Colonized Planets: #{new_model.num_planets}"
   end
 end
