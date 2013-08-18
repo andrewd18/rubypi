39baf443f1c575abfff6a2b6e886a5b1546cb813
Andrew Dorney <andrewd18@gmail.com>
2013-08-18 17:06:51 -0500

    Rework the industrial facility view so it has a left-to-right layout and shows more than just the combo boxes.

--- view/industrial_facility_view/industrial_facility_view.rb
+++ view/industrial_facility_view/industrial_facility_view.rb
@@ -3,6 +3,7 @@ require 'gtk3'
 require_relative '../../model/schematic.rb'
 require_relative '../common/select_building_combo_box.rb'
 require_relative '../common/building_image.rb'
+require_relative '../common/stored_products_widget.rb'
 
 require_relative '../gtk_helpers/simple_combo_box.rb'
 require_relative '../gtk_helpers/simple_table.rb'
@@ -34,59 +35,56 @@ class IndustrialFacilityView < Gtk::Box
 	bottom_row = Gtk::Box.new(:horizontal)
 	
 	# Create widgets.
-	# Left column.
-	schematic_label = Gtk::Label.new("Schematic:")
-	stored_products_label = Gtk::Label.new("Stored Products:")
-	input_from_label = Gtk::Label.new("Input from:")
-	output_to_label = Gtk::Label.new("Output to:")
 	
-	# Center column.
-	# Populate the combo box with the schematics this factory can accept.
+	# Input Building
+	input_building_label = Gtk::Label.new("Input Building")
+	@input_building_combo_box = SelectBuildingComboBox.new
+	@input_building_image = BuildingImage.new
+	@input_building_stored_products_widget = StoredProductsWidget.new(@controller)
+	input_building_vbox = Gtk::Box.new(:vertical)
+	input_building_vbox.pack_start(input_building_label, :expand => false)
+	input_building_vbox.pack_start(@input_building_combo_box, :expand => false)
+	input_building_vbox.pack_start(@input_building_image, :expand => false)
+	input_building_vbox.pack_start(@input_building_stored_products_widget, :expand => true)
+	
+	schematic_label = Gtk::Label.new("Schematic")
 	@schematic_combo_box = SimpleComboBox.new
+	@factory_building_image = BuildingImage.new
+	stored_products_label = Gtk::Label.new("Stored Products")
+	@stored_products_table = SimpleTable.new(3, 2)
+	@stored_products_vbox = Gtk::Box.new(:vertical)
+	@stored_products_vbox.pack_start(schematic_label, :expand => false)
+	@stored_products_vbox.pack_start(@schematic_combo_box, :expand => false)
+	@stored_products_vbox.pack_start(@factory_building_image, :expand => false)
+	@stored_products_vbox.pack_start(stored_products_label, :expand => false)
+	@stored_products_vbox.pack_start(@stored_products_table, :expand => false)
+	
+	
+	output_building_label = Gtk::Label.new("Output Building")
+	@output_building_combo_box = SelectBuildingComboBox.new
+	@output_building_image = BuildingImage.new
+	@output_building_stored_products_widget = StoredProductsWidget.new(@controller)
+	
+	output_building_vbox = Gtk::Box.new(:vertical)
+	output_building_vbox.pack_start(output_building_label, :expand => false)
+	output_building_vbox.pack_start(@output_building_combo_box, :expand => false)
+	output_building_vbox.pack_start(@output_building_image, :expand => false)
+	output_building_vbox.pack_start(@output_building_stored_products_widget, :expand => true)
 	
 	# Set up signals.
 	@on_schematic_change_signal = @schematic_combo_box.signal_connect("changed") do |combo_box|
 	  @controller.set_schematic_name(combo_box.selected_item)
 	end
 	
-	@input_building_combo_box = SelectBuildingComboBox.new
 	@on_input_building_change_signal = @input_building_combo_box.signal_connect("changed") do |combo_box|
 	  @controller.set_input_building(combo_box.selected_item)
 	end
 	
-	@output_building_combo_box = SelectBuildingComboBox.new
 	@on_output_building_change_signal = @output_building_combo_box.signal_connect("changed") do |combo_box|
 	  @controller.set_output_building(combo_box.selected_item)
 	end
 	
 	
-		                                # rows, columns, homogenous?
-	@factory_options_table = SimpleTable.new(4, 2, false)
-	
-	# Schematic row
-	@factory_options_table.attach(schematic_label, 1, 1)
-	@factory_options_table.attach(@schematic_combo_box, 1, 2)
-	
-	# Stored Products row
-	@factory_options_table.attach(stored_products_label, 2, 1)
-	# Middle row, right column (2, 2) is attached as part of create_sliders_for_stored_products,
-	
-	# Input From: row
-	@factory_options_table.attach(input_from_label, 3, 1)
-	@factory_options_table.attach(@input_building_combo_box, 3, 2)
-	
-	# Output to row
-	@factory_options_table.attach(output_to_label, 4, 1)
-	@factory_options_table.attach(@output_building_combo_box, 4, 2)
-	
-	# By wrapping the table in a vertical box, we ensure that the vbox expands
-	# and the table does not.
-	factory_options_table_vbox = Gtk::Box.new(:vertical)
-	factory_options_table_vbox.pack_start(@factory_options_table, :expand => false)
-	
-	# Finally, add a decorator frame around it.
-	factory_options_table_frame = Gtk::Frame.new
-	factory_options_table_frame.add(factory_options_table_vbox)
 	
 	
 	# Right column.
@@ -102,7 +100,9 @@ class IndustrialFacilityView < Gtk::Box
 	building_image_frame.add(building_image_column)
 	
 	# Pack columns left to right.
-	bottom_row.pack_start(factory_options_table_frame, :expand => true)
+	bottom_row.pack_start(input_building_vbox, :expand => false)
+	bottom_row.pack_start(@stored_products_vbox, :expand => true)
+	bottom_row.pack_start(output_building_vbox, :expand => false)
 	bottom_row.pack_start(building_image_frame, :expand => false)
 	
 	self.pack_start(bottom_row, :expand => true)
@@ -115,7 +115,16 @@ class IndustrialFacilityView < Gtk::Box
   def building_model=(new_building_model)
 	@building_model = new_building_model
 	
-	@building_image.building_model = new_building_model
+	# Change images.
+	@factory_building_image.building_model = new_building_model
+	
+	unless (@building_model.production_cycle_input_building == nil)
+	  @input_building_image.building_model = @building_model.production_cycle_input_building
+	end
+	
+	unless (@building_model.production_cycle_output_building == nil)
+	  @output_building_image.building_model = @building_model.production_cycle_output_building
+	end
 	
 	# WORKAROUND
 	# I wrap the view-update events in signal_handler_block(id) closures.
@@ -140,40 +149,36 @@ class IndustrialFacilityView < Gtk::Box
   end
   
   def create_sliders_for_stored_products
-	if (@stored_product_table)
-	  @stored_product_table.destroy
-	  # This automatically cleans up any packed children, namely the scales of @scales_hash.
-	end
-	
-	# Clear out the keys of the scales hash since values are dead.
 	if (@scales_hash)
+	  @stored_products_table.children.each do |child_widget|
+		child_widget.destroy
+	  end
+	  
+	  # Clear out the keys of the scales hash since values are dead.
 	  @scales_hash.clear
 	end
 	
-	# Table of stored products.
-	@stored_product_table = Gtk::Box.new(:vertical)
 	@scales_hash = Hash.new
 	
+	table_row_iter = 1
 	@building_model.stored_products.each_pair do |product_name, quantity|
 	  product_name_label = Gtk::Label.new("#{product_name}")
 	  
 	  max_allowed_storage_for_this_product = @building_model.schematic.inputs[product_name]
 	  product_quantity_scale = Gtk::Scale.new(:horizontal, 0, max_allowed_storage_for_this_product, 1)
 	  product_quantity_scale.value = quantity
+	  product_quantity_scale.width_request=(100)
 	  
 	  @scales_hash[product_name] = product_quantity_scale
 	  
-	  new_row = Gtk::Box.new(:horizontal)
-	  new_row.pack_start(product_name_label)
-	  new_row.pack_start(product_quantity_scale)
+	  @stored_products_table.attach(product_name_label, table_row_iter, 1)
+	  @stored_products_table.attach(product_quantity_scale, table_row_iter, 2)
 	  
-	  @stored_product_table.pack_start(new_row)
+	  table_row_iter += 1
 	end
 	
-	@factory_options_table.attach(@stored_product_table, 2, 2)
-	
 	# WORKAROUND
-	# If I don't force a show_all on @factory_options_table here, it won't show the new columns.
-	@factory_options_table.show_all
+	# If I don't force a show_all, the box won't show the new scales.
+	@stored_products_table.show_all
   end
 end
