595d9447f0bf66af52408e12e4ecbb9fa0cc8274
Andrew Dorney <andrewd18@gmail.com>
2013-08-29 17:01:06 -0500

    Add a planet export list.

--- view/pi_configuration_view/planet_list_row.rb
+++ view/pi_configuration_view/planet_list_row.rb
@@ -3,6 +3,7 @@ require 'gtk3'
 require_relative '../common/planet_image.rb'
 require_relative '../common/building_count_table.rb'
 require_relative 'planet_import_list.rb'
+require_relative 'planet_export_list.rb'
 
 class PlanetListRow < Gtk::Box
   def initialize(controller, planet_model)
@@ -30,7 +31,14 @@ class PlanetListRow < Gtk::Box
 	planet_imports_column.pack_start(imports_label, :expand => false)
 	planet_imports_column.pack_start(planet_import_list_scrolled_window, :expand => true)
 	
-	planet_export_list = Gtk::Label.new("Export List")
+	exports_label = Gtk::Label.new("Exports")
+	@planet_export_list = PlanetExportList.new(@planet_model)
+	planet_export_scrolled_window = Gtk::ScrolledWindow.new
+	planet_export_scrolled_window.set_policy(Gtk::PolicyType::NEVER, Gtk::PolicyType::AUTOMATIC)
+	planet_export_scrolled_window.add(@planet_export_list)
+	planet_exports_column = Gtk::Box.new(:vertical)
+	planet_exports_column.pack_start(exports_label, :expand => false)
+	planet_exports_column.pack_start(planet_export_scrolled_window, :expand => true)
 	
 	edit_button_column = Gtk::Box.new(:vertical)
 	edit_button = Gtk::Button.new(:label => "Edit")
@@ -55,7 +63,7 @@ class PlanetListRow < Gtk::Box
 	self.pack_start(planet_image_and_name_column, :padding => 5, :expand => false)
 	self.pack_start(@planet_buildings_box, :padding => 5, :expand => false)
 	self.pack_start(planet_imports_column, :padding => 5, :expand => true)
-	self.pack_start(planet_export_list, :padding => 5, :expand => true)
+	self.pack_start(planet_exports_column, :padding => 5, :expand => true)
 	self.pack_start(edit_button_column, :padding => 5, :expand => false)
 	self.pack_start(delete_button_column, :padding => 5, :expand => false)
 	
