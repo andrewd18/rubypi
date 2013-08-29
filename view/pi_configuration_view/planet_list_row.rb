2884155f2531ac7717a5bfef944ca5d7c1c4ef90
Andrew Dorney <andrewd18@gmail.com>
2013-08-29 16:56:56 -0500

    Add a planet import list.

--- view/pi_configuration_view/planet_list_row.rb
+++ view/pi_configuration_view/planet_list_row.rb
@@ -2,6 +2,7 @@ require 'gtk3'
 
 require_relative '../common/planet_image.rb'
 require_relative '../common/building_count_table.rb'
+require_relative 'planet_import_list.rb'
 
 class PlanetListRow < Gtk::Box
   def initialize(controller, planet_model)
@@ -18,7 +19,17 @@ class PlanetListRow < Gtk::Box
 	
 	@planet_buildings_box = BuildingCountTable.new(@planet_model)
 	
-	planet_import_list = Gtk::Label.new("Import List")
+	
+	
+	imports_label = Gtk::Label.new("Imports")
+	@planet_import_list = PlanetImportList.new(@planet_model)
+	planet_import_list_scrolled_window = Gtk::ScrolledWindow.new
+	planet_import_list_scrolled_window.set_policy(Gtk::PolicyType::NEVER, Gtk::PolicyType::AUTOMATIC)
+	planet_import_list_scrolled_window.add(@planet_import_list)
+	planet_imports_column = Gtk::Box.new(:vertical)
+	planet_imports_column.pack_start(imports_label, :expand => false)
+	planet_imports_column.pack_start(planet_import_list_scrolled_window, :expand => true)
+	
 	planet_export_list = Gtk::Label.new("Export List")
 	
 	edit_button_column = Gtk::Box.new(:vertical)
@@ -43,7 +54,7 @@ class PlanetListRow < Gtk::Box
 	
 	self.pack_start(planet_image_and_name_column, :padding => 5, :expand => false)
 	self.pack_start(@planet_buildings_box, :padding => 5, :expand => false)
-	self.pack_start(planet_import_list, :padding => 5, :expand => true)
+	self.pack_start(planet_imports_column, :padding => 5, :expand => true)
 	self.pack_start(planet_export_list, :padding => 5, :expand => true)
 	self.pack_start(edit_button_column, :padding => 5, :expand => false)
 	self.pack_start(delete_button_column, :padding => 5, :expand => false)
@@ -60,5 +71,6 @@ class PlanetListRow < Gtk::Box
 	# Push to complex children.
 	@planet_image.planet_model = @planet_model
 	@planet_buildings_box.planet_model = @planet_model
+	@planet_import_list.planet_model = @planet_model
   end
 end
